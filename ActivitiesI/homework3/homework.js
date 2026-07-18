7a41c31257d5215d5007414ca31bd00349774c48
Liwi <lighu.miranda@gmail.com>
2026-07-17 21:02:44 -0300

    Agregué un espacio

--- ActivitiesI/homework3/homework.js
+++ ActivitiesI/homework3/homework.js
@@ -14,7 +14,7 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
-}
+} 
 
 function invocarMetodo(objeto, metodo) {
   // "metodo" es una cadena que contiene el nombre de un método (funcion) en el objeto
